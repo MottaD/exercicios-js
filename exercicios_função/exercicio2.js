ff8c616a6f17bdde9302bc1244fe8c8ccf6b8879
Daniella Motta <101986844+MottaD@users.noreply.github.com>
2022-05-28 18:06:43 -0300

    main

--- exercicios_função/exercicio2.js
+++ exercicios_função/exercicio2.js
@@ -4,3 +4,17 @@
 //Equilátero: Os três lados são iguais
 //Isósceles: Dois lados iguais
 //Escaleno: Todos os lados são diferentes.
+
+function triangulos (lado1, lado2, lado3){
+    if(lado1 == lado2 && lado2 == lado3){
+        return 'Equilátero'
+    } else if (lado1 == lado2 || lado2 == lado3 || lado3 == lado1) {
+        return 'Isósceles'
+    } else {
+        return 'Escaleno'
+    }
+}
+
+console.log(triangulos(2,2,2))
+console.log(triangulos (2,2,4))
+console.log(triangulos(1,2,3))
